95baf595094a2c12fa5bf88d1240e36413e6946e
Arch Librarian <arch@canonical.com>
2004-09-20 16:55:10 +0000

    Actually fixed md5 Author: jgg Date: 1999-11-16 03:10:56 GMT Actually fixed md5

--- apt-pkg/contrib/md5.cc
+++ apt-pkg/contrib/md5.cc
@@ -1,6 +1,6 @@
 // -*- mode: cpp; mode: fold -*-
 // Description								/*{{{*/
-// $Id: md5.cc,v 1.9 1999/11/16 01:51:03 jgg Exp $
+// $Id: md5.cc,v 1.10 1999/11/16 03:10:56 jgg Exp $
 /* ######################################################################
    
    MD5Sum - MD5 Message Digest Algorithm.
@@ -54,7 +54,7 @@
 // ---------------------------------------------------------------------
 /* Swap n 32 bit longs in given buffer */
 #ifdef WORDS_BIGENDIAN
-static void byteSwap(uint8_t *buf, unsigned words)
+static void byteSwap(uint32_t *buf, unsigned words)
 {
    uint8_t *p = (uint8_t *)buf;
    
@@ -269,7 +269,7 @@ bool MD5Summation::Add(const unsigned char *data,unsigned long len)
 
    // First chunk is an odd size
    memcpy((unsigned char *)in + 64 - t,data,t);
-   byteSwap((uint8_t *)in, 16);
+   byteSwap(in, 16);
    MD5Transform(buf,in);
    data += t;
    len -= t;
@@ -278,7 +278,7 @@ bool MD5Summation::Add(const unsigned char *data,unsigned long len)
    while (len >= 64)
    {
       memcpy(in,data,64);
-      byteSwap((uint8_t *)in,16);
+      byteSwap(in,16);
       MD5Transform(buf,in);
       data += 64;
       len -= 64;
@@ -334,20 +334,20 @@ MD5SumValue MD5Summation::Result()
       if (count < 0) 
       {
 	 memset(p,0,count + 8);
-	 byteSwap((uint8_t *)in, 16);
+	 byteSwap(in, 16);
 	 MD5Transform(buf,in);
 	 p = (unsigned char *)in;
 	 count = 56;
       }
       
       memset(p, 0, count);
-      byteSwap((uint8_t *)in, 14);
+      byteSwap(in, 14);
       
       // Append length in bits and transform
       in[14] = bytes[0] << 3;
       in[15] = bytes[1] << 3 | bytes[0] >> 29;
       MD5Transform(buf,in);   
-      byteSwap((uint8_t *)buf,4);
+      byteSwap(buf,4);
       Done = true;
    }
    
