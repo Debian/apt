92173b19af439ac49d842902a3a57105d396d7d8
Arch Librarian <arch@canonical.com>
2004-09-20 16:51:12 +0000

    gzip method Author: jgg Date: 1998-10-25 07:07:29 GMT gzip method

--- methods/gzip.cc
+++ methods/gzip.cc
@@ -1,21 +1,24 @@
 // -*- mode: cpp; mode: fold -*-
 // Description								/*{{{*/
-// $Id: copy.cc,v 1.2 1998/10/25 07:07:29 jgg Exp $
+// $Id: gzip.cc,v 1.1 1998/10/25 07:07:30 jgg Exp $
 /* ######################################################################
 
-   Copy URI - This method takes a uri like a file: uri and copies it
-   to the destination file.
+   GZip method - Take a file URI in and decompress it into the target 
+   file.
    
    ##################################################################### */
 									/*}}}*/
 // Include Files							/*{{{*/
 #include <apt-pkg/fileutl.h>
-#include <strutl.h>
 #include <apt-pkg/error.h>
+#include <apt-pkg/configuration.h>
+#include <apt-pkg/acquire-worker.h>
+#include <strutl.h>
 
 #include <sys/stat.h>
-#include <utime.h>
 #include <unistd.h>
+#include <utime.h>
+#include <wait.h>
 #include <stdio.h>
 									/*}}}*/
 
@@ -42,8 +45,9 @@ int main()
    
    printf("100 Capabilities\n"
 	  "Version: 1.0\n"
-	  "Pipeline: true\n\n");
-      
+	  "Pipeline: true\n"
+	  "Send-Config: true\n\n");
+
    vector<string> Messages;   
    while (1)
    {
@@ -65,7 +69,14 @@ int main()
 	    return 100;
 	 }
 	 
-	 // We only understand 600 URI Fetch messages
+	 // 601 configuration message
+	 if (Number == 601)
+	 {
+	    pkgInjectConfiguration(Message,*_config);
+	    continue;
+	 }	 
+
+	 // 600 URI Fetch message
 	 if (Number != 600)
 	    continue;
 	 
@@ -98,15 +109,45 @@ int main()
 	    continue;
 	 }	 
 	 
-	 // Copy the file
-	 if (CopyFile(From,To) == false)
+	 // Fork gzip
+	 int Process = fork();
+	 if (Process < 0)
 	 {
+	    _error->Errno("fork","Couldn't fork gzip");
+	    Fail(URI);
+	    continue;
+	 }
+	 
+	 // The child
+	 if (Process == 0)
+	 {
+	    dup2(From.Fd(),STDIN_FILENO);
+	    dup2(To.Fd(),STDOUT_FILENO);
+	    
+	    const char *Args[3];
+	    Args[0] = _config->FindFile("Dir::bin::gzip","gzip").c_str();
+	    Args[1] = "-d";
+	    Args[2] = 0;
+	    execvp(Args[0],(char **)Args);
+	 }
+	 From.Close();
+	 To.Close();
+	 
+	 // Wait for gzip to finish
+	 int Status;
+	 if (waitpid(Process,&Status,0) != Process)
+	 {
+	    _error->Errno("wait","Waiting for gzip failed");
 	    Fail(URI);
 	    continue;
 	 }	 
-	
-	 From.Close();
-	 To.Close();
+
+	 if (WIFEXITED(Status) == 0 || WEXITSTATUS(Status) != 0)
+	 {
+	    _error->Error("gzip failed, perhaps the disk is full or the directory permissions are wrong.");
+	    Fail(URI);
+	    continue;
+	 }	 
 	 
 	 // Transfer the modification times
 	 struct stat Buf;
@@ -125,7 +166,7 @@ int main()
 	    Fail(URI);
 	    continue;
 	 }
-	       
+	 
 	 // Send the message
 	 Result += "\n\n";
 	 if (write(STDOUT_FILENO,Result.begin(),Result.length()) != 
