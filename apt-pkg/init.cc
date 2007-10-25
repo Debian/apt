c5597900026cf124654b808c800c4731a2eb655c
Otavio Salvador <otavio@ossystems.com.br>
2007-10-25 09:25:02 -0200

    * Applied patch from Daniel Leidert <daniel.leidert@wgdd.de> to fix   APT::Acquire::Translation "none" support, closes: #437523.

--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -72,7 +72,10 @@ bool pkgInitConfig(Configuration &Cnf)
    // State   
    Cnf.Set("Dir::Log","var/log/apt");
    Cnf.Set("Dir::Log::Terminal","term.log");
-   
+
+   // Translation
+   Cnf.Set("APT::Acquire::Translation", "environment");
+
    bool Res = true;
    
    // Read an alternate config file
@@ -104,9 +107,6 @@ bool pkgInitConfig(Configuration &Cnf)
    }
 #endif
 
-   // Translation
-   Cnf.Set("APT::Acquire::Translation", "environment");
-   
    return true;
 }
 									/*}}}*/
