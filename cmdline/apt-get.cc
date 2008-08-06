496a05c628d39c981a8f472f16629043d8508808
Michael Vogt <michael.vogt@ubuntu.com>
2008-08-06 13:58:44 +0200

    make "apt-get build-dep" installed packages marked automatic by default. This can be changed by setting the value of APT::Get::Build-Dep-Automatic to false (thanks to Aaron Haviland, closes: #44874, LP: #248268)

--- cmdline/apt-get.cc
+++ cmdline/apt-get.cc
@@ -2513,6 +2513,8 @@ bool DoBuildDep(CommandLine &CmdL)
             {
                // We successfully installed something; skip remaining alternatives
                skipAlternatives = hasAlternatives;
+	       if(_config->FindB("APT::Get::Build-Dep-Automatic", true) == true)
+		  Cache->MarkAuto(Pkg, true);
                continue;
             }
             else if (hasAlternatives)
