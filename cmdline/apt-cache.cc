6013c6f4f09c1aedf373654dfadac398f0e0a91d
Eugene V. Lyubimkin <jackyf@1501-debian>
2008-11-02 11:23:24 +0200

    Fixed typo.

--- cmdline/apt-cache.cc
+++ cmdline/apt-cache.cc
@@ -1740,7 +1740,7 @@ bool ShowHelp(CommandLine &Cmd)
       "   depends - Show raw dependency information for a package\n"
       "   rdepends - Show reverse dependency information for a package\n"
       "   pkgnames - List the names of all packages in the system\n"
-      "   dotty - Generate package graphs for GraphVis\n"
+      "   dotty - Generate package graphs for GraphViz\n"
       "   xvcg - Generate package graphs for xvcg\n"
       "   policy - Show policy settings\n"
       "\n"
