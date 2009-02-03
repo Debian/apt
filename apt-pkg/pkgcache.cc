71a9e7cf8b7c78950939bb866701d8445fe5ba4c
Michael Vogt <michael.vogt@ubuntu.com>
2009-02-03 12:24:31 +0100

    * apt-pkg/pkgcache.cc:   - do not run "dpkg --configure pkg" if pkg is in trigger-awaited     state (LP: #322955)

--- apt-pkg/pkgcache.cc
+++ apt-pkg/pkgcache.cc
@@ -274,9 +274,13 @@ pkgCache::PkgIterator::OkState pkgCache::PkgIterator::State() const
       return NeedsUnpack;
    
    if (Pkg->CurrentState == pkgCache::State::UnPacked ||
-       Pkg->CurrentState == pkgCache::State::HalfConfigured ||
-       Pkg->CurrentState == pkgCache::State::TriggersPending ||
-       Pkg->CurrentState == pkgCache::State::TriggersAwaited)
+       Pkg->CurrentState == pkgCache::State::HalfConfigured) ||
+      //we don't need to care for triggers awaiting packages
+      //dpkg will deal with them automatically when the 
+      //trigger pending action is run (those packages are usually
+      //in half-configured or triggers-pending state)
+      //Pkg->CurrentState == pkgCache::State::TriggersAwaited
+       Pkg->CurrentState == pkgCache::State::TriggersPending)
       return NeedsConfigure;
    
    if (Pkg->CurrentState == pkgCache::State::HalfInstalled ||
