217d575b5713f4d9275177b58a36c581a7880c03
Michael Vogt <michael.vogt@ubuntu.com>
2009-07-14 11:22:41 +0200

    * apt-pkg/acquire-worker.cc:   - show error details of failed methods * apt-pkg/contrib/fileutl.cc:   - if a process aborts with signal, show signal number * methods/http.cc:   - ignore SIGPIPE, we deal with EPIPE from write in     HttpMethod::ServerDie() (LP: #385144)

--- methods/http_main.cc
+++ methods/http_main.cc
@@ -1,5 +1,6 @@
 #include <apt-pkg/fileutl.h>
 #include <apt-pkg/acquire-method.h>
+#include <signal.h>
 
 #include "connect.h"
 #include "rfc2553emu.h"
@@ -9,6 +10,10 @@
 int main()
 {
    setlocale(LC_ALL, "");
+
+   // ignore SIGPIPE, this can happen on write() if the socket
+   // closes the connection (this is dealt with via ServerDie())
+   signal(SIGPIPE, SIG_IGN);
 
    HttpMethod Mth;
    return Mth.Loop();
