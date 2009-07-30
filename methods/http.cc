056539eeb8eb84cea65c0c15466111b978ee156d
Michael Vogt <michael.vogt@ubuntu.com>
2009-07-30 18:25:16 +0200

    methods/http.cc: remove merge artifact

--- methods/http.cc
+++ methods/http.cc
@@ -1316,15 +1316,4 @@ int HttpMethod::Loop()
 }
 									/*}}}*/
 
-int main()
-{
-   setlocale(LC_ALL, "");
-   // ignore SIGPIPE, this can happen on write() if the socket
-   // closes the connection (this is dealt with via ServerDie())
-   signal(SIGPIPE, SIG_IGN);
-
-   HttpMethod Mth;
-   return Mth.Loop();
-}
-
 
