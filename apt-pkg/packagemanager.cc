f66a64c26cb60656255c89fcd4fc757422d9545a
David Kalnischkies <kalnischkies@gmail.com>
2009-10-01 20:32:55 +0200

    improve the documentation and errormessages regarding a failing immediate configuration as the old message was quite a bit scare and uninformative.

--- apt-pkg/packagemanager.cc
+++ apt-pkg/packagemanager.cc
@@ -471,7 +471,8 @@ bool pkgPackageManager::SmartUnPack(PkgIterator Pkg)
       List->Flag(Pkg,pkgOrderList::UnPacked,pkgOrderList::States);
       if (List->IsFlag(Pkg,pkgOrderList::Immediate) == true)
 	 if (SmartConfigure(Pkg) == false)
-	    return _error->Error("Internal Error, Could not perform immediate configuration (1) on %s",Pkg.Name());
+	    return _error->Error(_("Could not perform immediate configuration for on already unpacked %s."
+			"Please see man 5 apt.conf under APT::Immediate-Configure for details."),Pkg.Name());
       return true;
    }
 
@@ -571,7 +572,8 @@ bool pkgPackageManager::SmartUnPack(PkgIterator Pkg)
    // Perform immedate configuration of the package.
    if (List->IsFlag(Pkg,pkgOrderList::Immediate) == true)
       if (SmartConfigure(Pkg) == false)
-	 return _error->Error("Internal Error, Could not perform immediate configuration (2) on %s",Pkg.Name());
+	 return _error->Error(_("Could not perform immediate configuration on %s."
+			"Please see man 5 apt.conf under APT::Immediate-Configure for details."),Pkg.Name());
    
    return true;
 }
