4bfbcbba96460450cc269dc91ff46dbd1a34271e
Julian Andres Klode <jak@debian.org>
2009-11-23 20:46:39 +0100

    CMake/Translations.cmake: Sort the list of languages.

--- CMake/Translations.cmake
+++ CMake/Translations.cmake
@@ -3,30 +3,27 @@
 
 macro(apt_add_translation_domain domain files)	
 	# Create the template for this specific sub-domain
-	add_custom_command (
-		OUTPUT ${PROJECT_BINARY_DIR}/${domain}.pot
+	add_custom_command (OUTPUT ${PROJECT_BINARY_DIR}/${domain}.pot
 		COMMAND xgettext --add-comments --foreign -k_ -kN_
 		                 -o ${PROJECT_BINARY_DIR}/${domain}.pot ${files}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 	)
-	
 	file(GLOB translations "${PROJECT_SOURCE_DIR}/po/*.po")
-	
+	list(SORT translations)
 	foreach(file ${translations})
 		get_filename_component(langcode ${file} NAME_WE)
 		set(outdir ${PROJECT_BINARY_DIR}/locale/${langcode}/LC_MESSAGES)
 		file(MAKE_DIRECTORY ${outdir})
-		
 		# Command to merge and compile the messages
-		add_custom_command(
-			OUTPUT ${outdir}/${domain}.mo
+		add_custom_command(OUTPUT ${outdir}/${domain}.mo
 			COMMAND msgmerge -qo - ${file} ${PROJECT_BINARY_DIR}/${domain}.pot |
 					msgfmt -o ${outdir}/${domain}.mo -
 			DEPENDS ${file} ${PROJECT_BINARY_DIR}/${domain}.pot
 		)
 		
 		set(mofiles ${mofiles} ${outdir}/${domain}.mo)
-		install(FILES ${outdir}/${domain}.mo DESTINATION "share/locale/${langcode}/LC_MESSAGES")
+		install(FILES ${outdir}/${domain}.mo
+		        DESTINATION "share/locale/${langcode}/LC_MESSAGES")
 	endforeach(file ${translations})
 	
 	add_custom_target(nls-${domain} ALL DEPENDS ${mofiles})
