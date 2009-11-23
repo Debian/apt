ae2f6e8c0f5a2829eb7d5d58ea996eb03164efa3
Julian Andres Klode <jak@debian.org>
2009-11-23 17:28:09 +0100

    Cleanup config.h and completely drop support for systems without sys/statvfs.h.

--- buildlib/config.h.cmake
+++ buildlib/config.h.cmake
@@ -2,29 +2,8 @@
    byte first (like Motorola and SPARC, unlike Intel and VAX).  */
 #cmakedefine WORDS_BIGENDIAN
 
-/* The following 4 are only used by inttypes.h shim if the system lacks
-   inttypes.h */
-/* The number of bytes in a usigned char.  */
-#cmakedefine SIZEOF_CHAR
-
-/* The number of bytes in a unsigned int.  */
-#cmakedefine SIZEOF_INT
-
-/* The number of bytes in a unsigned long.  */
-#cmakedefine SIZEOF_LONG
-
-/* The number of bytes in a unsigned short.  */
-#cmakedefine SIZEOF_SHORT
-
 /* Define if we have the timegm() function */
 #cmakedefine HAVE_TIMEGM
-
-/* These two are used by the statvfs shim for glibc2.0 and bsd */
-/* Define if we have sys/vfs.h */
-#cmakedefine HAVE_VFS_H
-
-/* Define if we have sys/mount.h */
-#cmakedefine HAVE_MOUNT_H
 
 /* Define if we have enabled pthread support */
 #cmakedefine HAVE_PTHREAD
