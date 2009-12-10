30fa74369085863842efed5e86054dd3a1d7b05a
Michael Vogt <mvo@debian.org>
2009-12-10 23:48:04 +0100

    * apt-pkg/init.h:   - add compatibilty with old ABI name until the next ABI break

--- apt-pkg/init.h
+++ apt-pkg/init.h
@@ -21,7 +21,9 @@
 // reverse-dependencies of libapt-pkg against the new SONAME.
 // Non-ABI-Breaks should only increase RELEASE number.
 // See also buildlib/libversion.mak
-#define APT_PKG_MAJOR 4
+// FIXME: this needs to be changed to "4" (without quotes) on the next
+//        ABI break
+#define APT_PKG_MAJOR libc6.9-6-4
 #define APT_PKG_MINOR 8
 #define APT_PKG_RELEASE 0
     
