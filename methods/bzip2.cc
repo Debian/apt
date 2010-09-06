cf1cdb3328dacde829db8bbb3db4f5373ac07d61
David Kalnischkies <kalnischkies@gmail.com>
2010-09-06 12:17:15 +0200

    * methods/{gzip,bzip2}.cc:   - empty files can never be valid archives (Closes: #595691)

--- methods/bzip2.cc
+++ methods/bzip2.cc
@@ -56,12 +56,9 @@ bool Bzip2Method::Fetch(FetchItem *Itm)
    // Open the source and destination files
    FileFd From(Path,FileFd::ReadOnly);
 
-   // if the file is empty, just rename it and return
-   if(From.Size() == 0) 
-   {
-      rename(Path.c_str(), Itm->DestFile.c_str());
-      return true;
-   }
+   // FIXME add an error message saying that empty files can't be valid archives
+   if(From.Size() == 0)
+      return false;
 
    int GzOut[2];   
    if (pipe(GzOut) < 0)
