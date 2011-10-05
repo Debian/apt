7cb28948317e0d326c8663ec3c9ce995d5bf65e8
David Kalnischkies <kalnischkies@gmail.com>
2011-10-05 22:45:22 +0200

    * apt-pkg/deb/debmetaindex.cc:   - none is a separator, not a language: no need for Index (Closes: #624218) * apt-pkg/aptconfiguration.cc:   - do not builtin languages only if none is forced (Closes: #643787)

--- apt-pkg/deb/debmetaindex.cc
+++ apt-pkg/deb/debmetaindex.cc
@@ -13,6 +13,7 @@
 #include <apt-pkg/error.h>
 
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
@@ -201,7 +202,11 @@ vector <struct IndexTarget *>* debReleaseIndex::ComputeIndexTargets() const {
 		}
 	}
 
-	std::vector<std::string> const lang = APT::Configuration::getLanguages(true);
+	std::vector<std::string> lang = APT::Configuration::getLanguages(true);
+	std::vector<std::string>::iterator lend = std::remove(lang.begin(), lang.end(), "none");
+	if (lend != lang.end())
+		lang.erase(lend);
+
 	if (lang.empty() == true)
 		return IndexTargets;
 
@@ -213,7 +218,6 @@ vector <struct IndexTarget *>* debReleaseIndex::ComputeIndexTargets() const {
 		     s != sections.end(); ++s) {
 			for (std::vector<std::string>::const_iterator l = lang.begin();
 			     l != lang.end(); ++l) {
-				if (*l == "none") continue;
 				IndexTarget * Target = new OptionalIndexTarget();
 				Target->ShortDesc = "Translation-" + *l;
 				Target->MetaKey = TranslationIndexURISuffix(l->c_str(), *s);
