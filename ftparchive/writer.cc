8e5d47a3a4e735835498574a58e0a33de04a0377
David Kalnischkies <kalnischkies@gmail.com>
2012-07-04 22:50:09 +0200

    * ftparchive/writer.cc:   - handle the APT::FTPArchive::Packages::SHA512 option correctly instead     of overriding SHA256, thanks Christian Marillat! (Closes: #680252)

--- ftparchive/writer.cc
+++ ftparchive/writer.cc
@@ -318,7 +318,7 @@ PackagesWriter::PackagesWriter(string const &DB,string const &Overrides,string c
    DoMD5 = _config->FindB("APT::FTPArchive::Packages::MD5",DoMD5);
    DoSHA1 = _config->FindB("APT::FTPArchive::Packages::SHA1",DoSHA1);
    DoSHA256 = _config->FindB("APT::FTPArchive::Packages::SHA256",DoSHA256);
-   DoSHA256 = _config->FindB("APT::FTPArchive::Packages::SHA512",true);
+   DoSHA512 = _config->FindB("APT::FTPArchive::Packages::SHA512",DoSHA512);
    DoAlwaysStat = _config->FindB("APT::FTPArchive::AlwaysStat", false);
    DoContents = _config->FindB("APT::FTPArchive::Contents",true);
    NoOverride = _config->FindB("APT::FTPArchive::NoOverrideMsg",false);
