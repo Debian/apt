27cc55ee824913aa3687cc3adf91d96e89bd27d0
David Kalnischkies <kalnischkies@gmail.com>
2013-05-11 10:10:07 +0200

    non-inline RunGPGV methods to restore ABI compatibility with previous versions to fix partial upgrades (Closes: #707771)
    The rename in 0.9.7.9~exp2 moved the method body to the class definition
    which means it became inline, which isn't ABI compatibile. The reverse of
    moving inline to non-inline is safe though.

--- apt-pkg/indexcopy.cc
+++ apt-pkg/indexcopy.cc
@@ -642,6 +642,18 @@ bool SigVerify::CopyAndVerify(string CDROM,string Name,vector<string> &SigList,	
    return true;
 }
 									/*}}}*/
+// SigVerify::RunGPGV - deprecated wrapper calling ExecGPGV		/*{{{*/
+bool SigVerify::RunGPGV(std::string const &File, std::string const &FileOut,
+      int const &statusfd, int fd[2]) {
+   ExecGPGV(File, FileOut, statusfd, fd);
+   return false;
+};
+bool SigVerify::RunGPGV(std::string const &File, std::string const &FileOut,
+      int const &statusfd) {
+   ExecGPGV(File, FileOut, statusfd);
+   return false;
+};
+									/*}}}*/
 bool TranslationsCopy::CopyTranslations(string CDROM,string Name,	/*{{{*/
 				vector<string> &List, pkgCdromStatus *log)
 {
