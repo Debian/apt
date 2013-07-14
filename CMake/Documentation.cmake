6bf5ef0076efa9900920e6e83909f0e48db0d050
Julian Andres Klode <jak@debian.org>
2013-07-14 23:36:17 +0200

    CMake/Documentation.cmake: Relicense under the MIT/Expat license
    This is just in case someone wants to reuse this later on. Also fix the
    po4a macro to use the type argument.

--- CMake/Documentation.cmake
+++ CMake/Documentation.cmake
@@ -1,5 +1,24 @@
 # Copyright (C) 2009 Julian Andres Klode <jak@debian.org>.
-# Licensed under the same terms as APT; i.e. GPL 2 or later.
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 macro(add_debiandoc target sourcefiles installdest)
 	foreach(file ${sourcefiles})
@@ -25,7 +44,7 @@ endmacro(add_debiandoc target sourcefiles installdest)
 
 macro(add_po4a type master po target deps)
 	add_custom_command(OUTPUT ${target}
-		COMMAND po4a-translate --keep 0 -f docbook -m ${master}
+		COMMAND po4a-translate --keep 0 -f ${type} -m ${master}
 		                       -p ${po} -l ${target}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 		DEPENDS ${deps} ${master} ${po})
