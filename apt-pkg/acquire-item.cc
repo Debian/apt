c3a17127ad383ec8e1f480ff2e1a4dccaa537a11
David Kalnischkies <david@kalnischkies.de>
2013-12-27 14:52:15 +0100

    reenable unlimited pdiff files download
    In 51fc6def77edfb1f429a48e5169519e9e05a759b we limited the amount of
    pdiff to be downloaded per index to 20. This was a compromise between
    not letting it go overboard (becoming even slower) and not using
    bandwidth needlessly. Now that with the POC the speed reason is gone it
    makes sense again to download as much files as we possible can via pdiff
    to save bandwidth (and possibly even time).
    
    It also avoids problems with the limit in cases we were we deal with a
    server merged archieve as this limit assumes a strict patch progression.

--- apt-pkg/acquire-item.cc
+++ apt-pkg/acquire-item.cc
@@ -462,7 +462,7 @@ bool pkgAcqDiffIndex::ParseDiffIndex(string IndexDiffFile)		/*{{{*/
 	 if (available_patches.empty() == false)
 	 {
 	    // patching with too many files is rather slow compared to a fast download
-	    unsigned long const fileLimit = _config->FindI("Acquire::PDiffs::FileLimit", 20);
+	    unsigned long const fileLimit = _config->FindI("Acquire::PDiffs::FileLimit", 0);
 	    if (fileLimit != 0 && fileLimit < available_patches.size())
 	    {
 	       if (Debug)
