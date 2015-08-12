10b39ae62db1f0d15a208a286cc86aff06c2fb69
Julian Andres Klode <jak@debian.org>
2015-08-12 13:07:56 +0200

    Add a parameter ConsiderFiles to GetPriority(VerIterator)
    This allows us to exclude files from being considered for the
    priority, so it will return only specific-version matches.

--- apt-pkg/policy.cc
+++ apt-pkg/policy.cc
@@ -368,11 +368,12 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::PkgIterator const &Pkg)
       return Pins[Pkg->ID].Priority;
    return 0;
 }
-APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver)
+APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver, bool considerFiles)
 {
    if (VerPins[Ver->ID].Type != pkgVersionMatch::None)
       return VerPins[Ver->ID].Priority;
-
+   if (!considerFiles)
+      return 0;
 
    int priority = std::numeric_limits<int>::min();
    for (pkgCache::VerFileIterator file = Ver.FileList(); file.end() == false; file++)
