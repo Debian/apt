05e7d4eee1c1089762538b4d9a7494652b1ee23c
Julian Andres Klode <jak@debian.org>
2015-10-30 15:41:13 +0100

    thread-safety tests: Adjust path to lists
    Gbp-Dch: ignore

--- test/thread-safety/not-thread-safe.sh
+++ test/thread-safety/not-thread-safe.sh
@@ -1,7 +1,7 @@
-git grep -p --color -n -E "$(grep -v ^# thread-check.txt  | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
+git grep -p --color -n -E "$(grep -v ^# test/thread-safety/thread-check.txt  | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
     apt-inst/ apt-pkg/  | ansi2html  | ssh alioth.debian.org 'cat > /home/groups/apt/htdocs/not-thread-safe/index.html'
-git grep -p --color -n -E "$(grep ^# thread-check.txt  | cut -f2 -d# | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
+git grep -p --color -n -E "$(grep ^# test/thread-safety/thread-check.txt  | cut -f2 -d# | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
     apt-inst/ apt-pkg/  | ansi2html  | ssh alioth.debian.org 'cat > /home/groups/apt/htdocs/not-thread-safe/portable.html'
 
-git grep -p --color -n -E "$(grep -v ^# thread-check-internal.txt  | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
+git grep -p --color -n -E "$(grep -v ^# test/thread-safety/thread-check-internal.txt  | sed 's/(.*/\\\\ *\\\\(/' | xargs | tr ' ' '|')" \
     apt-inst/ apt-pkg/  | ansi2html  | ssh alioth.debian.org 'cat > /home/groups/apt/htdocs/not-thread-safe/internal.html'
