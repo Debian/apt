9596cb945aee8ab78f37b925e0691a7cc1488d56
Julian Andres Klode <jak@debian.org>
2015-12-11 15:00:17 +0100

    apt-internal-solver: Make ShowHelp() and GetCommands() static
    This fixes a warning reported by gcc.
    
    Gbp-Dch: ignore

--- cmdline/apt-internal-solver.cc
+++ cmdline/apt-internal-solver.cc
@@ -41,7 +41,7 @@
 #include <apti18n.h>
 									/*}}}*/
 
-bool ShowHelp(CommandLine &)						/*{{{*/
+static bool ShowHelp(CommandLine &)					/*{{{*/
 {
 	std::cout <<
 		_("Usage: apt-internal-solver\n"
@@ -58,7 +58,7 @@ APT_NORETURN static void DIE(std::string const &message) {		/*{{{*/
 	exit(EXIT_FAILURE);
 }
 									/*}}}*/
-std::vector<aptDispatchWithHelp> GetCommands()				/*{{{*/
+static std::vector<aptDispatchWithHelp> GetCommands()			/*{{{*/
 {
    return {};
 }
