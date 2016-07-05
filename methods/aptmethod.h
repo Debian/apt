366021988e2c7a7a6ca29d4f6876bb1e6c8b181f
David Kalnischkies <david@kalnischkies.de>
2016-07-05 20:04:27 +0200

    don't change owner/perms/times through file:// symlinks
    If we have files in partial/ from a previous invocation or similar such
    those could be symlinks created by file:// sources. The code is
    expecting only real files through and happily changes owner,
    modification times and permission on the file the symlink points to
    which tend to be files we have no business in touching in this way.
    Permissions of symlinks shouldn't be changed, changing owner is usually
    pointless to, but just to be sure we pick the easy way out and use
    lchown, check for symlinks before chmod/utimes.
    
    Reported-By: Mattia Rizzolo on IRC
    (cherry picked from commit 3465138575e1fd0d5892d9b6be1ae232eb873460)

--- methods/aptmethod.h
+++ methods/aptmethod.h
@@ -3,8 +3,16 @@
 
 #include <apt-pkg/acquire-method.h>
 #include <apt-pkg/configuration.h>
+#include <apt-pkg/error.h>
 
 #include <string>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <apti18n.h>
 
 class aptMethod : public pkgAcqMethod
 {
@@ -40,6 +48,29 @@ public:
       va_start(args,Format);
       PrintStatus("104 Warning", Format, args);
       va_end(args);
+   }
+
+   bool TransferModificationTimes(char const * const From, char const * const To, time_t &LastModified)
+   {
+      if (strcmp(To, "/dev/null") == 0)
+	 return true;
+
+      struct stat Buf2;
+      if (lstat(To, &Buf2) != 0 || S_ISLNK(Buf2.st_mode))
+	 return true;
+
+      struct stat Buf;
+      if (stat(From, &Buf) != 0)
+	 return _error->Errno("stat",_("Failed to stat"));
+
+      // we don't use utimensat here for compatibility reasons: #738567
+      struct timeval times[2];
+      times[0].tv_sec = Buf.st_atime;
+      LastModified = times[1].tv_sec = Buf.st_mtime;
+      times[0].tv_usec = times[1].tv_usec = 0;
+      if (utimes(To, times) != 0)
+	 return _error->Errno("utimes",_("Failed to set modification time"));
+      return true;
    }
 
    aptMethod(char const * const Binary, char const * const Ver, unsigned long const Flags) :
