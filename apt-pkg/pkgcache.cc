1236419d67e119acc7c0df48f8b14a277e0b5683
Julian Andres Klode <jak@debian.org>
2016-09-27 18:36:10 +0200

    Bump the cache major version for non-backportable changes
    We already have two stable series with major version 10, and
    the next commits will introduce non-backportable performance
    changes that affect the cache algorithms, so we need to bump
    the major version now to prevent future problems.

--- apt-pkg/pkgcache.cc
+++ apt-pkg/pkgcache.cc
@@ -58,8 +58,8 @@ pkgCache::Header::Header()
 
    /* Whenever the structures change the major version should be bumped,
       whenever the generator changes the minor version should be bumped. */
-   APT_HEADER_SET(MajorVersion, 10);
-   APT_HEADER_SET(MinorVersion, 7);
+   APT_HEADER_SET(MajorVersion, 11);
+   APT_HEADER_SET(MinorVersion, 0);
    APT_HEADER_SET(Dirty, false);
 
    APT_HEADER_SET(HeaderSz, sizeof(pkgCache::Header));
