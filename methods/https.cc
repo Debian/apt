9229494fa1744d26ed4b116d9cc3512c60e1a1da
Julian Andres Klode <jak@debian.org>
2016-12-29 14:16:07 +0100

    https: Quote path in URL before passing it to curl
    Curl requires URLs to be urlencoded. We are however giving it
    undecoded URLs. This causes it go completely nuts if there is
    a space in the URI, producing requests like:
    
        GET /a file HTTP/1.1
    
    which the servers then interpret as a GET request for "/a" with
    HTTP version "file" or some other non-sense.
    
    This works around the issue by encoding the path component of
    the URL. I'm not sure if we should encode other parts of the URL
    as well, this one seems to do the trick for the actual issue at
    hand.
    
    A more correct fix is to avoid the dequoting and (re-)quoting
    of URLs when a redirect occurs / a new request is sent. That's
    been on the radar for probably a year or two now, but nobody
    bothered implementing that yet.
    
    LP: #1651923
    (cherry picked from commit 994515e689dcc5f963f5fed58284831750a5da03)
    (cherry picked from commit 438b1d78b4c33d0a97406f0a7071e3c413dc0aa3)

--- methods/https.cc
+++ methods/https.cc
@@ -241,6 +241,10 @@ bool HttpsMethod::Fetch(FetchItem *Itm)
 
    maybe_add_auth (Uri, _config->FindFile("Dir::Etc::netrc"));
 
+   // The "+" is encoded as a workaround for a amazon S3 bug
+   // see LP bugs #1003633 and #1086997. (taken from http method)
+   Uri.Path = QuoteString(Uri.Path, "+~ ");
+
    FetchResult Res;
    CURLUserPointer userp(this, &Res, Itm);
    // callbacks
