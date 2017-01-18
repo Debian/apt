33f982b90a4f77be18cb82daf8c79e9c5513761c
David Kalnischkies <david@kalnischkies.de>
2017-01-18 11:31:13 +0100

    don't show update stats if cache generation is disabled
    Unlikely that anyone is actually running into this, but if we asked to
    not generate a cache and avoid it in the first step we shouldn't create
    one implicitly anyway by displaying the statistics.

--- apt-private/private-update.cc
+++ apt-private/private-update.cc
@@ -71,13 +71,13 @@ bool DoUpdate(CommandLine &CmdL)
       ListUpdate(Stat, *List);
    }
 
+   if (_config->FindB("pkgCacheFile::Generate", true) == false)
+      return true;
+
    // Rebuild the cache.
-   if (_config->FindB("pkgCacheFile::Generate", true) == true)
-   {
-      pkgCacheFile::RemoveCaches();
-      if (Cache.BuildCaches() == false)
-	 return false;
-   }
+   pkgCacheFile::RemoveCaches();
+   if (Cache.BuildCaches() == false)
+      return false;
 
    // show basic stats (if the user whishes)
    if (_config->FindB("APT::Cmd::Show-Update-Stats", false) == true)
