f2d33afaffb3cc76d96a8ac6bbc5bcb765103e29
Julian Andres Klode <jak@debian.org>
2017-03-07 17:33:54 +0100

    Ignore \.ucf-[a-z]+$ like we do for \.dpkg-[a-z]+$
    This gets rid of warnings about .ucf-dist files
    
    Reported-By: Axel Beckert (on IRC)
    (cherry picked from commit 5094697fe4b2459ff6f706a22006d3028369f3fa)
    (cherry picked from commit 0c42bab8534b4dc95dabdff2a8e08a3574291ec0)

--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -81,6 +81,7 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.disabled$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.bak$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.dpkg-[a-z]+$");
+   Cnf.Set("Dir::Ignore-Files-Silently::", "\\.ucf-[a-z]+$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.save$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.orig$");
    Cnf.Set("Dir::Ignore-Files-Silently::", "\\.distUpgrade$");
