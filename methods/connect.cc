d3a70c3e5ae68a0e5a3d4667dd1d0fc0887e6263
Julian Andres Klode <jak@debian.org>
2017-06-30 11:02:54 +0200

    Reset failure reason when connection was successful
    When APT was trying multiple addresses, any later error
    somewhere else would be reported with ConnectionRefused
    or ConnectionTimedOut as the FailReason because that
    was set by early connect attempts. This causes APT to
    handle the failures differently, leading to some weirdly
    breaking test cases (like the changed one).
    
    Add debugging to the previously failing test case so
    we can find out when something goes wrong there again.

--- methods/connect.cc
+++ methods/connect.cc
@@ -179,7 +179,9 @@ static bool DoConnect(struct addrinfo *Addr, std::string const &Host,
       return _error->Errno("connect",_("Could not connect to %s:%s (%s)."),Host.c_str(),
 			   Service,Name);
    }
-   
+
+   Owner->SetFailReason("");
+
    return true;
 }
 									/*}}}*/
