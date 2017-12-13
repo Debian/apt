fd9f75b3164cd4eb52783eed3be9898dd7fba800
David Kalnischkies <david@kalnischkies.de>
2017-12-13 15:21:40 +0100

    use the newest available git-clang-format in PATH
    Hardcoding a specific version is sad as default versions change over
    time, so instead of tying us to a specific clang version we let the
    script figure out a good version by looking at what is available in
    PATH.
    
    Gbp-Dch: Ignore

--- git-clang-format.sh
+++ git-clang-format.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-git clang-format-3.8 --diff "$@" | sed "s#+/\*\}\}\}\*/#+									/*}}}*/#" | patch -p1
+CLANG_FORMAT="$(find $(echo "$PATH" | tr ':' ' ') -name 'git-clang-format-*' | sort | tail -n1 )"
+if [ -z "$CLANG_FORMAT" ]; then
+   echo >&2 'Could not find a clang-format to use. Is the package clang-format installed?'
+   exit 1
+fi
+git "$(basename "$CLANG_FORMAT" | cut -d'-' -f 2-)" --diff "$@" | \
+   sed "s#+/\*\}\}\}\*/#+									/*}}}*/#" | \
+   patch -p1
