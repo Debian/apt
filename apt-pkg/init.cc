412fff10af76ff8a77cee3fe10fb1ba8a91e860b
Julian Andres Klode <julian.klode@canonical.com>
2018-03-15 11:14:04 +0100

    Use https for Ubuntu changelogs
    We just enabled https on changelogs.ubuntu.com, let's use it.

--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -207,7 +207,7 @@ bool pkgInitConfig(Configuration &Cnf)
 
    Cnf.CndSet("Acquire::Changelogs::URI::Origin::Debian", "http://metadata.ftp-master.debian.org/changelogs/@CHANGEPATH@_changelog");
    Cnf.CndSet("Acquire::Changelogs::URI::Origin::Tanglu", "http://metadata.tanglu.org/changelogs/@CHANGEPATH@_changelog");
-   Cnf.CndSet("Acquire::Changelogs::URI::Origin::Ubuntu", "http://changelogs.ubuntu.com/changelogs/pool/@CHANGEPATH@/changelog");
+   Cnf.CndSet("Acquire::Changelogs::URI::Origin::Ubuntu", "https://changelogs.ubuntu.com/changelogs/pool/@CHANGEPATH@/changelog");
    Cnf.CndSet("Acquire::Changelogs::URI::Origin::Ultimedia", "http://packages.ultimediaos.com/changelogs/pool/@CHANGEPATH@/changelog.txt");
    Cnf.CndSet("Acquire::Changelogs::AlwaysOnline::Origin::Ubuntu", true);
 
