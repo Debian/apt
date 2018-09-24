Debian/apt

commit b2e465d6d32d2dc884f58b94acb7e35f671a87fe

Join with aliencode Author: jgg Date: 2001-02-20 07:03:16 GMT Join with aliencode

apt-pkg/cachefile.cc
@@ -1,6 +1,6 @@
 // -*- mode: cpp; mode: fold -*-
 // Description								/*{{{*/
-// $Id: cachefile.cc,v 1.4 1999/06/24 04:06:30 jgg Exp $
+// $Id: cachefile.cc,v 1.5 2001/02/20 07:03:17 jgg Exp $
 /* ######################################################################
    
    CacheFile - Simple wrapper class for opening, generating and whatnot
@@ -21,23 +21,29 @@
 #include <apt-pkg/sourcelist.h>
 #include <apt-pkg/pkgcachegen.h>
 #include <apt-pkg/configuration.h>
+#include <apt-pkg/policy.h>
+#include <apt-pkg/pkgsystem.h>
+    
+#include <apti18n.h>
 									/*}}}*/
 
 // CacheFile::CacheFile - Constructor					/*{{{*/
 // ---------------------------------------------------------------------
 /* */
-pkgCacheFile::pkgCacheFile() : Map(0), Cache(0), Lock(0) 
+pkgCacheFile::pkgCacheFile() : Map(0), Cache(0), DCache(0), Policy(0)
 {
 }
 									/*}}}*/
-// CacheFile::~CacheFile - Destructor						/*{{{*/
+// CacheFile::~CacheFile - Destructor					/*{{{*/
 // ---------------------------------------------------------------------
 /* */
 pkgCacheFile::~pkgCacheFile()
 {
+   delete DCache;
+   delete Policy;
    delete Cache;
    delete Map;
-   delete Lock;
+   _system->UnLock(true);
 }   
 									/*}}}*/
 // CacheFile::Open - Open the cache files, creating if necessary	/*{{{*/
@@ -46,7 +52,8 @@ pkgCacheFile::~pkgCacheFile()
 bool pkgCacheFile::Open(OpProgress &Progress,bool WithLock)
 {
    if (WithLock == true)
-      Lock = new pkgDpkgLock;
+      if (_system->Lock() == false)
+	 return false;
    
    if (_error->PendingError() == true)
       return false;
@@ -54,42 +61,57 @@ bool pkgCacheFile::Open(OpProgress &Progress,bool WithLock)
    // Read the source list
    pkgSourceList List;
    if (List.ReadMainList() == false)
-      return _error->Error("The list of sources could not be read.");
+      return _error->Error(_("The list of sources could not be read."));
+
+   // Read the caches
+   bool Res = pkgMakeStatusCache(List,Progress,&Map,!WithLock);
+   Progress.Done();
+   if (Res == false)
+      return _error->Error(_("The package lists or status file could not be parsed or opened."));
+
+   /* This sux, remove it someday */
+   if (_error->empty() == false)
+      _error->Warning(_("You may want to run apt-get update to correct these missing files"));
+
+   Cache = new pkgCache(Map);
+   if (_error->PendingError() == true)
+      return false;
    
-   /* Build all of the caches, using the cache files if we are locking 
-      (ie as root) */
-   if (WithLock == true)
-   {
-      pkgMakeStatusCache(List,Progress);
-      Progress.Done();
-      if (_error->PendingError() == true)
-	 return _error->Error("The package lists or status file could not be parsed or opened.");
-      if (_error->empty() == false)
-	 _error->Warning("You may want to run apt-get update to correct these missing files");
-      
-      // Open the cache file
-      FileFd File(_config->FindFile("Dir::Cache::pkgcache"),FileFd::ReadOnly);
-      if (_error->PendingError() == true)
-	 return false;
-      
-      Map = new MMap(File,MMap::Public | MMap::ReadOnly);
-      if (_error->PendingError() == true)
-	 return false;
-   }
-   else
-   {
-      Map = pkgMakeStatusCacheMem(List,Progress);
-      Progress.Done();
-      if (Map == 0)
-	 return false;
-   }
+   // The policy engine
+   Policy = new pkgPolicy(Cache);
+   if (_error->PendingError() == true)
+      return false;
+   if (ReadPinFile(*Policy) == false)
+      return false;
    
    // Create the dependency cache
-   Cache = new pkgDepCache(*Map,Progress);
+   DCache = new pkgDepCache(Cache,Policy);
+   if (_error->PendingError() == true)
+      return false;
+   
+   DCache->Init(&Progress);
    Progress.Done();
    if (_error->PendingError() == true)
       return false;
    
    return true;
+}
+									/*}}}*/
+
+// CacheFile::Close - close the cache files				/*{{{*/
+// ---------------------------------------------------------------------
+/* */
+void pkgCacheFile::Close()
+{
+   delete DCache;
+   delete Policy;
+   delete Cache;
+   delete Map;
+   _system->UnLock(true);
+
+   Map = 0;
+   DCache = 0;
+   Policy = 0;
+   Cache = 0;
 }
 									/*}}}*/

apt-pkg/cachefile.h
@@ -1,6 +1,6 @@
 // -*- mode: cpp; mode: fold -*-
 // Description								/*{{{*/
-// $Id: cachefile.h,v 1.3 1999/06/27 03:18:28 jgg Exp $
+// $Id: cachefile.h,v 1.4 2001/02/20 07:03:17 jgg Exp $
 /* ######################################################################
    
    CacheFile - Simple wrapper class for opening, generating and whatnot
@@ -8,6 +8,9 @@
    This class implements a simple 2 line mechanism to open various sorts
    of caches. It can operate as root, as not root, show progress and so on,
    it transparently handles everything necessary.
+   
+   This means it can rebuild caches from the source list and instantiates
+   and prepares the standard policy mechanism.
    
    ##################################################################### */
 									/*}}}*/
@@ -19,30 +22,32 @@
 #endif 
 
 #include <apt-pkg/depcache.h>
-#include <apt-pkg/dpkginit.h>
 
+class pkgPolicy;
 class pkgCacheFile
 {
    protected:
    
    MMap *Map;
-   pkgDepCache *Cache;
-   pkgDpkgLock *Lock;
+   pkgCache *Cache;
+   pkgDepCache *DCache;
    
    public:
+
+   pkgPolicy *Policy;
       
    // We look pretty much exactly like a pointer to a dep cache
-   inline operator pkgDepCache &() {return *Cache;};
-   inline operator pkgDepCache *() {return Cache;};
-   inline pkgDepCache *operator ->() {return Cache;};
-   inline pkgDepCache &operator *() {return *Cache;};
-   inline pkgDepCache::StateCache &operator [](pkgCache::PkgIterator const &I) {return (*Cache)[I];};
-   inline unsigned char &operator [](pkgCache::DepIterator const &I) {return (*Cache)[I];};
+   inline operator pkgCache &() {return *Cache;};
+   inline operator pkgCache *() {return Cache;};
+   inline operator pkgDepCache &() {return *DCache;};
+   inline operator pkgDepCache *() {return DCache;};
+   inline pkgDepCache *operator ->() {return DCache;};
+   inline pkgDepCache &operator *() {return *DCache;};
+   inline pkgDepCache::StateCache &operator [](pkgCache::PkgIterator const &I) {return (*DCache)[I];};
+   inline unsigned char &operator [](pkgCache::DepIterator const &I) {return (*DCache)[I];};
 
-   // Release the dpkg status lock
-   inline void ReleaseLock() {Lock->Close();};
-   
    bool Open(OpProgress &Progress,bool WithLock = true);
+   void Close();
    
    pkgCacheFile();
    ~pkgCacheFile();

commit e02297b8e22dae04872fe6fab6dba966de65dbba

pkgCacheFile: Only unlock in destructor if locked before
pkgCacheFile's destructor unlocks the system, which is confusing
if you did not open the cachefile with WithLock set. Create a private
data instance that holds the value of WithLock.

This regression was introduced in commit b2e465d6d32d2dc884f58b94acb7e35f671a87fe:

    Join with aliencode
    Author: jgg
    Date: 2001-02-20 07:03:16 GMT
    Join with aliencode

by replacing a "Lock" member that was only initialized when the lock
was taken by calls to Lock, UnLock; with the latter also taking place
if the former did not occur.

Regression-Of: b2e465d6d32d2dc884f58b94acb7e35f671a87fe
LP: #1794053

apt-pkg/cachefile.cc
@@ -35,12 +35,18 @@
 
 #include <apti18n.h>
 									/*}}}*/
+
+struct pkgCacheFile::Private
+{
+   bool WithLock = false;
+};
+
 // CacheFile::CacheFile - Constructor					/*{{{*/
-pkgCacheFile::pkgCacheFile() : d(NULL), ExternOwner(false), Map(NULL), Cache(NULL),
+pkgCacheFile::pkgCacheFile() : d(new Private()), ExternOwner(false), Map(NULL), Cache(NULL),
 				DCache(NULL), SrcList(NULL), Policy(NULL)
 {
 }
-pkgCacheFile::pkgCacheFile(pkgDepCache * const Owner) : d(NULL), ExternOwner(true),
+pkgCacheFile::pkgCacheFile(pkgDepCache * const Owner) : d(new Private()), ExternOwner(true),
    Map(&Owner->GetCache().GetMap()), Cache(&Owner->GetCache()),
    DCache(Owner), SrcList(NULL), Policy(NULL)
 {
@@ -59,8 +65,10 @@ pkgCacheFile::~pkgCacheFile()
    }
    delete Policy;
    delete SrcList;
-   if (ExternOwner == false)
+   if (d->WithLock == true)
       _system->UnLock(true);
+
+   delete d;
 }
 									/*}}}*/
 // CacheFile::BuildCaches - Open and build the cache files		/*{{{*/
@@ -97,8 +105,11 @@ bool pkgCacheFile::BuildCaches(OpProgress *Progress, bool WithLock)
    }
 
    if (WithLock == true)
+   {
       if (_system->Lock() == false)
 	 return false;
+      d->WithLock = true;
+   }
 
    if (_error->PendingError() == true)
       return false;
@@ -337,7 +348,11 @@ void pkgCacheFile::Close()
       ExternOwner = false;
    delete Policy;
    delete SrcList;
-   _system->UnLock(true);
+   if (d->WithLock == true)
+   {
+      _system->UnLock(true);
+      d->WithLock = false;
+   }
 
    Map = NULL;
    DCache = NULL;

apt-pkg/cachefile.h
@@ -36,8 +36,9 @@ class OpProgress;
 
 class pkgCacheFile
 {
+   struct Private;
    /** \brief dpointer placeholder (for later in case we need it) */
-   void * const d;
+   Private *const d;
    bool ExternOwner;
 
    protected: