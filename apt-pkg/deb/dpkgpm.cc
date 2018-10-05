0ce6bc5ec70ff84eb2829f91f286a93687e77a46
Julian Andres Klode <julian.klode@canonical.com>
2018-10-05 12:29:39 +0200

    Set DPKG_FRONTEND_LOCKED when running {pre,post}-invoke scripts
    Some post-invoke scripts install packages, which fails because
    the environment variable is not set. This sets the variable for
    all three kinds of scripts {pre,post-}invoke and pre-install-pkgs,
    but we will only allow post-invoke at a later time.
    
    Gbp-Dch: full
    (cherry picked from commit 6675601c81de85b40dc89772c1d6d17f1811c5ba)
    LP: #1796808

--- apt-pkg/deb/dpkgpm.cc
+++ apt-pkg/deb/dpkgpm.cc
@@ -480,6 +480,9 @@ bool pkgDPkgPM::RunScriptsWithPkgs(const char *Cnf)
 	 strprintf(hookfd, "%d", InfoFD);
 	 setenv("APT_HOOK_INFO_FD", hookfd.c_str(), 1);
 
+	 if (_system != nullptr && _system->IsLocked() == true && stringcasecmp(Cnf, "DPkg::Pre-Install-Pkgs") == 0)
+	    setenv("DPKG_FRONTEND_LOCKED", "true", 1);
+
 	 debSystem::DpkgChrootDirectory();
 	 const char *Args[4];
 	 Args[0] = "/bin/sh";
