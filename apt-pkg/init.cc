8f503447a4e5e494487672e4678498e288f6d9be
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2018-10-26 01:31:10 +0200

    Remove "Tanglu" config for Acquire::Changelogs and as vendor
    This is an inactive derivative according to the census, and all the URLs which
    are part of tanglu.org are dead.

--- apt-pkg/init.cc
+++ apt-pkg/init.cc
@@ -205,7 +205,6 @@ bool pkgInitConfig(Configuration &Cnf)
    Cnf.CndSet("Acquire::IndexTargets::deb-src::Sources::Optional", false);
 
    Cnf.CndSet("Acquire::Changelogs::URI::Origin::Debian", "https://metadata.ftp-master.debian.org/changelogs/@CHANGEPATH@_changelog");
-   Cnf.CndSet("Acquire::Changelogs::URI::Origin::Tanglu", "http://metadata.tanglu.org/changelogs/@CHANGEPATH@_changelog");
    Cnf.CndSet("Acquire::Changelogs::URI::Origin::Ubuntu", "https://changelogs.ubuntu.com/changelogs/pool/@CHANGEPATH@/changelog");
    Cnf.CndSet("Acquire::Changelogs::AlwaysOnline::Origin::Ubuntu", true);
 
