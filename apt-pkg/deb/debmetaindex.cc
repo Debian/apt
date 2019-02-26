97bd7e492bad1ef50f2659faef61b409ffe67311
Julian Andres Klode <julian.klode@canonical.com>
2019-02-26 12:53:04 +0100

    metaindex: Remove deprecated checkSum::Hash (replaced by Hashes)

--- apt-pkg/deb/debmetaindex.cc
+++ apt-pkg/deb/debmetaindex.cc
@@ -510,9 +510,6 @@ bool debReleaseIndex::Load(std::string const &Filename, std::string * const Erro
             Sum->MetaKeyFilename = Name;
             Sum->Size = Size;
 	    Sum->Hashes.FileSize(Size);
-	    APT_IGNORE_DEPRECATED_PUSH
-	    Sum->Hash = hs;
-	    APT_IGNORE_DEPRECATED_POP
 	    Entries[Name] = Sum;
          }
          Entries[Name]->Hashes.push_back(hs);
