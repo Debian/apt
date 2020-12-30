83aeda7548cf5862275da695b732b9299ca189a8
David Kalnischkies <david@kalnischkies.de>
2020-12-30 23:50:34 +0100

    Pick security-suite name based on debian stable version
    The suite name in buster was buster/updates but it will change to
    bullseye-security in bullseye. We will update these various data points
    closer to the actual release to not confuse users of unstable and of
    downstreams pulling from there. This does implement an automatic switch
    for the security suite name though based on the metadata as I don't
    trust myself/us to remember to do that at the right moment.
    
    Closes: #969932

--- vendor/debian/apt-vendor.ent.sh
+++ vendor/debian/apt-vendor.ent.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+set -e
+
+BASEDIR="$(readlink -f "$(dirname "$0")")"
+
+cat <<EOF
 <!-- details about the keys used by the distribution -->
 <!ENTITY keyring-distro "Debian">
 <!ENTITY keyring-package "<package>debian-archive-keyring</package>">
@@ -6,7 +12,16 @@
 <!ENTITY keyring-master-filename "">
 <!ENTITY keyring-uri "">
 <!ENTITY vendor-codename "&debian-stable-codename;">
-<!ENTITY vendor-security-suite "&vendor-codename;/updates">
+
+EOF
+
+if dpkg --compare-versions "$("${BASEDIR}/../getinfo" 'debian-stable-version')" '>=' '11'; then
+	echo '<!ENTITY vendor-security-suite "&vendor-codename;-security">'
+else
+	echo '<!ENTITY vendor-security-suite "&vendor-codename;/updates">'
+fi
+
+cat <<EOF
 
 <!ENTITY sourceslist-list-format "deb http://deb.debian.org/debian &vendor-codename; main contrib non-free
 deb http://security.debian.org &vendor-security-suite; main contrib non-free">
@@ -19,3 +34,4 @@ Types: deb
 URIs: http://security.debian.org
 Suites: &vendor-security-suite;
 Components: main contrib non-free">
+EOF
