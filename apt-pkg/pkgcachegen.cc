Debian/apt

commit 2c03eed649068c8838d971ceeb1a09603dcc8e9c

ftparchive/: prepare for APT::StringView -> std::string_view

ftparchive/override.cc
@@ -3,9 +3,9 @@
 /* ######################################################################
 
    Override
-   
+
    Store the override file.
-   
+
    ##################################################################### */
 									/*}}}*/
 // Include Files							/*{{{*/
@@ -31,11 +31,11 @@ bool Override::ReadOverride(string const &File,bool const &Source)
 {
    if (File.empty() == true)
       return true;
-   
+
    FILE *F = fopen(File.c_str(),"r");
    if (F == 0)
       return _error->Errno("fopen",_("Unable to open %s"),File.c_str());
-   
+
    char Line[1000];
    unsigned long long Counter = 0;
    while (fgets(Line,sizeof(Line),F) != 0)
@@ -43,7 +43,7 @@ bool Override::ReadOverride(string const &File,bool const &Source)
       Counter++;
       Item Itm;
 
-      // Silence 
+      // Silence
       for (char *I = Line; *I != 0; I++)
 	 if (*I == '#')
 	    *I = 0;
@@ -113,10 +113,10 @@ bool Override::ReadOverride(string const &File,bool const &Source)
 	 {
 	    *End = 0;
 	    Itm.OldMaint = _strstrip(Start);
-	    
+
 	    End += 3;
 	    Itm.NewMaint = _strstrip(End);
-	 }	 
+	 }
       }
 
       Mapping[Pkg] = Itm;
@@ -135,18 +135,18 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 {
    if (File.empty() == true)
       return true;
-   
+
    FILE *F = fopen(File.c_str(),"r");
    if (F == 0)
       return _error->Errno("fopen",_("Unable to open %s"),File.c_str());
-  
+
    char Line[1000];
    unsigned long long Counter = 0;
    while (fgets(Line,sizeof(Line),F) != 0)
    {
       Counter++;
 
-      // Silence 
+      // Silence
       for (char *I = Line; *I != 0; I++)
 	 if (*I == '#')
 	    *I = 0;
@@ -165,7 +165,7 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 	 _error->Warning(_("Malformed override %s line %llu #1"),File.c_str(),
 			 Counter);
 	 continue;
-      }      
+      }
       *End = 0;
 
       // Find the field
@@ -179,8 +179,8 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 	 continue;
       }
       *End = 0;
-      
-      // Find the field value 
+
+      // Find the field value
       for (End++; isspace(*End) != 0 && *End != 0; End++);
       char *Value = End;
       for (; *End != 0; End++);
@@ -190,7 +190,7 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 	 _error->Warning(_("Malformed override %s line %llu #3"),File.c_str(),
 			 Counter);
 	 continue;
-      }      
+      }
       *End = 0;
 
       Mapping[Pkg].FieldOverride[Field] = Value;
@@ -208,10 +208,10 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 /* Returns a override item for the given package and the given architecture.
  * Treats "all" special
  */
-Override::Item* Override::GetItem(string const &Package, string const &Architecture)
+Override::Item* Override::GetItem(std::string_view const &Package, std::string_view const &Architecture)
 {
    map<string,Item>::const_iterator I = Mapping.find(Package);
-   map<string,Item>::iterator J = Mapping.find(Package + "/" + Architecture);
+   map<string,Item>::iterator J = Mapping.find((std::string{Package} += '/') += Architecture);
 
    if (I == Mapping.end() && J == Mapping.end())
    {
@@ -246,17 +246,17 @@ Override::Item* Override::GetItem(string const &Package, string const &Architect
    there is a rule but it does not match then the empty string is returned,
    also if there was no rewrite rule the empty string is returned. Failed
    indicates if there was some kind of problem while rewriting. */
-string Override::Item::SwapMaint(string const &Orig,bool &Failed)
+string Override::Item::SwapMaint(std::string_view const &Orig,bool &Failed)
 {
    Failed = false;
-   
+
    // Degenerate case..
    if (NewMaint.empty() == true)
 	 return OldMaint;
-   
+
    if (OldMaint == "*")
       return NewMaint;
-   
+
    /* James: ancient, eliminate it, however it is still being used in the main
       override file. Thus it persists.*/
 #if 1
@@ -264,13 +264,13 @@ string Override::Item::SwapMaint(string const &Orig,bool &Failed)
    string::const_iterator End = OldMaint.begin();
    while (1)
    {
-      string::const_iterator Start = End;      
+      string::const_iterator Start = End;
       for (; End < OldMaint.end() &&
-	   (End + 3 >= OldMaint.end() || End[0] != ' ' || 
+	   (End + 3 >= OldMaint.end() || End[0] != ' ' ||
 	    End[1] != '/' || End[2] != '/'); ++End);
       if (stringcasecmp(Start,End,Orig.begin(),Orig.end()) == 0)
 	 return NewMaint;
-      
+
       if (End >= OldMaint.end())
 	 break;
 
@@ -279,9 +279,9 @@ string Override::Item::SwapMaint(string const &Orig,bool &Failed)
    }
 #else
    if (stringcasecmp(OldMaint.begin(),OldMaint.end(),Orig.begin(),Orig.end()) == 0)
-      return NewMaint;   
+      return NewMaint;
 #endif
-   
+
    Failed = true;
    return string();
 }

ftparchive/override.h
@@ -3,9 +3,9 @@
 /* ######################################################################
 
    Override
-   
+
    Store the override file.
-   
+
    ##################################################################### */
 									/*}}}*/
 #ifndef OVERRIDE_H
@@ -16,11 +16,11 @@
 
 using std::string;
 using std::map;
-    
+
 class Override
 {
    public:
-   
+
    struct Item
    {
       string Priority;
@@ -28,21 +28,21 @@ class Override
       string NewMaint;
 
       map<string,string> FieldOverride;
-      string SwapMaint(string const &Orig,bool &Failed);
+      string SwapMaint(std::string_view const &Orig,bool &Failed);
       ~Item() {};
    };
-   
-   map<string,Item> Mapping;
-   
-   inline Item *GetItem(string const &Package) 
+
+   map<string,Item,std::less<>> Mapping;
+
+   inline Item *GetItem(std::string_view const &Package)
    {
       return GetItem(Package, "");
    }
-   Item *GetItem(string const &Package, string const &Architecture);
-   
+   Item *GetItem(std::string_view const &Package, std::string_view const &Architecture);
+
    bool ReadOverride(string const &File,bool const &Source = false);
    bool ReadExtraOverride(string const &File,bool const &Source = false);
 };
 
 #endif
-    
+

commit 323fcee3730fc8e3edd555f458bf260382f6d3ef

std::string_view const & -> std::string_view
Fixes: 2c03eed649068c8838d971ceeb1a09603dcc8e9c

apt-pkg/pkgcachegen.cc
@@ -370,7 +370,7 @@ bool pkgCacheGenerator::MergeListPackage(ListParser &List, pkgCache::PkgIterator
 									/*}}}*/
 // CacheGenerator::MergeListVersion					/*{{{*/
 bool pkgCacheGenerator::MergeListVersion(ListParser &List, pkgCache::PkgIterator &Pkg,
-					 std::string_view const &Version, pkgCache::VerIterator* &OutVer)
+					 std::string_view Version, pkgCache::VerIterator* &OutVer)
 {
    pkgCache::VerIterator Ver = Pkg.VersionList();
    Dynamic<pkgCache::VerIterator> DynVer(Ver);
@@ -866,7 +866,7 @@ bool pkgCacheGenerator::NewFileVer(pkgCache::VerIterator &Ver,
 // ---------------------------------------------------------------------
 /* This puts a version structure in the linked list */
 map_pointer<pkgCache::Version> pkgCacheGenerator::NewVersion(pkgCache::VerIterator &Ver,
-					    std::string_view const &VerStr,
+					    std::string_view VerStr,
 					    map_pointer<pkgCache::Package> const ParentPkg,
 					    uint32_t Hash,
 					    map_pointer<pkgCache::Version> const Next)

apt-pkg/pkgcachegen.h
@@ -1,18 +1,18 @@
 // -*- mode: cpp; mode: fold -*-
 // Description								/*{{{*/
 /* ######################################################################
-   
+
    Package Cache Generator - Generator for the cache structure.
-   
-   This builds the cache structure from the abstract package list parser. 
+
+   This builds the cache structure from the abstract package list parser.
    Each archive source has it's own list parser that is instantiated by
-   the caller to provide data for the generator. 
-   
+   the caller to provide data for the generator.
+
    Parts of the cache are created by this generator class while other
    parts are created by the list parser. The list parser is responsible
    for creating version, depends and provides structures, and some of
    their contents
-   
+
    ##################################################################### */
 									/*}}}*/
 #ifndef PKGLIB_PKGCACHEGEN_H
@@ -116,7 +116,7 @@ class APT_HIDDEN pkgCacheGenerator					/*{{{*/
 
    bool NewGroup(pkgCache::GrpIterator &Grp, std::string_view Name);
    bool NewPackage(pkgCache::PkgIterator &Pkg, std::string_view Name, std::string_view Arch);
-   map_pointer<pkgCache::Version> NewVersion(pkgCache::VerIterator &Ver, std::string_view const &VerStr,
+   map_pointer<pkgCache::Version> NewVersion(pkgCache::VerIterator &Ver, std::string_view VerStr,
 			    map_pointer<pkgCache::Package> const ParentPkg, uint32_t Hash,
 			    map_pointer<pkgCache::Version> const Next);
    map_pointer<pkgCache::Description> NewDescription(pkgCache::DescIterator &Desc,const std::string &Lang, std::string_view md5sum,map_stringitem_t const idxmd5str);
@@ -162,7 +162,7 @@ class APT_HIDDEN pkgCacheGenerator					/*{{{*/
    APT_HIDDEN bool MergeListGroup(ListParser &List, std::string const &GrpName);
    APT_HIDDEN bool MergeListPackage(ListParser &List, pkgCache::PkgIterator &Pkg);
    APT_HIDDEN bool MergeListVersion(ListParser &List, pkgCache::PkgIterator &Pkg,
-			 std::string_view const &Version, pkgCache::VerIterator* &OutVer);
+			 std::string_view Version, pkgCache::VerIterator* &OutVer);
 
    APT_HIDDEN bool AddImplicitDepends(pkgCache::GrpIterator &G, pkgCache::PkgIterator &P,
 			   pkgCache::VerIterator &V);
@@ -200,7 +200,7 @@ class APT_HIDDEN pkgCacheListParser
    bool NewProvidesAllArch(pkgCache::VerIterator &Ver, std::string_view Package,
 			   std::string_view Version, uint8_t const Flags);
    public:
-   
+
    // These all operate against the current section
    virtual std::string Package() = 0;
    virtual bool ArchitectureAll() = 0;
@@ -220,9 +220,9 @@ class APT_HIDDEN pkgCacheListParser
 			   pkgCache::VerIterator &Ver) = 0;
    virtual map_filesize_t Offset() = 0;
    virtual map_filesize_t Size() = 0;
-   
+
    virtual bool Step() = 0;
-   
+
    virtual bool CollectFileProvides(pkgCache &/*Cache*/,
 				    pkgCache::VerIterator &/*Ver*/) {return true;};
 

ftparchive/override.cc
@@ -208,7 +208,7 @@ bool Override::ReadExtraOverride(string const &File,bool const &/*Source*/)
 /* Returns a override item for the given package and the given architecture.
  * Treats "all" special
  */
-Override::Item* Override::GetItem(std::string_view const &Package, std::string_view const &Architecture)
+Override::Item* Override::GetItem(std::string_view Package, std::string_view Architecture)
 {
    map<string,Item>::const_iterator I = Mapping.find(Package);
    map<string,Item>::iterator J = Mapping.find((std::string{Package} += '/') += Architecture);
@@ -246,7 +246,7 @@ Override::Item* Override::GetItem(std::string_view const &Package, std::string_v
    there is a rule but it does not match then the empty string is returned,
    also if there was no rewrite rule the empty string is returned. Failed
    indicates if there was some kind of problem while rewriting. */
-string Override::Item::SwapMaint(std::string_view const &Orig,bool &Failed)
+string Override::Item::SwapMaint(std::string_view Orig,bool &Failed)
 {
    Failed = false;
 

ftparchive/override.h
@@ -28,17 +28,17 @@ class Override
       string NewMaint;
 
       map<string,string> FieldOverride;
-      string SwapMaint(std::string_view const &Orig,bool &Failed);
+      string SwapMaint(std::string_view Orig,bool &Failed);
       ~Item() {};
    };
 
    map<string,Item,std::less<>> Mapping;
 
-   inline Item *GetItem(std::string_view const &Package)
+   inline Item *GetItem(std::string_view Package)
    {
       return GetItem(Package, "");
    }
-   Item *GetItem(std::string_view const &Package, std::string_view const &Architecture);
+   Item *GetItem(std::string_view Package, std::string_view Architecture);
 
    bool ReadOverride(string const &File,bool const &Source = false);
    bool ReadExtraOverride(string const &File,bool const &Source = false);

ftparchive/writer.cc
@@ -549,7 +549,7 @@ TranslationWriter::TranslationWriter(string const &File, string const &TransComp
 // TranslationWriter::DoPackage - Process a single package		/*{{{*/
 // ---------------------------------------------------------------------
 /* Create a Translation-Master file for this Packages file */
-bool TranslationWriter::DoPackage(string_view const &Pkg, string const &Desc,
+bool TranslationWriter::DoPackage(string_view Pkg, string const &Desc,
 				  string const &MD5)
 {
    if (Output == NULL)

ftparchive/writer.h
@@ -95,7 +95,7 @@ class TranslationWriter
    FileFd *Output;
 
    public:
-   bool DoPackage(std::string_view const &Pkg, string const &Desc, string const &MD5);
+   bool DoPackage(std::string_view Pkg, string const &Desc, string const &MD5);
 
    TranslationWriter(string const &File, string const &TransCompress, mode_t const &Permissions);
    ~TranslationWriter();