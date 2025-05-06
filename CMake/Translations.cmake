9a7e34d491b828a227930654d4f99e4fd2eb93ec
Julian Andres Klode <jak@debian.org>
2025-05-06 18:11:33 +0200

    Pass --check to msgfmt and fix broken format strings
    The check option discovers broken format string translations as
    well as other issues like broken headers, enable it.
    
    The Japanese translation was discovered to both use the wrong
    ordering for %.*s translations with different positions, and
    missing the dot, e.g.
    
        %1$*2$s should have been %2$.*1$s
    
        (the number after % matches the string and the number after
         .* the size)

--- CMake/Translations.cmake
+++ CMake/Translations.cmake
@@ -114,7 +114,7 @@ function(apt_add_translation_domain)
             DEPENDS ${file} ${CMAKE_CURRENT_BINARY_DIR}/${domain}.pot-tmp
         )
         add_custom_command(OUTPUT ${outdir}/${domain}.mo
-            COMMAND msgfmt --statistics -o ${outdir}/${domain}.mo  ${outdir}/${domain}.po
+            COMMAND msgfmt --check --statistics -o ${outdir}/${domain}.mo  ${outdir}/${domain}.po
             DEPENDS ${outdir}/${domain}.po
         )
 
@@ -179,7 +179,7 @@ function(apt_add_po_statistics excluded)
             add_custom_command(
                 TARGET statistics PRE_BUILD
                 COMMAND printf "%-6s " "${langcode}:"
-                COMMAND msgfmt --statistics -o /dev/null ${translation}
+                COMMAND msgfmt --check --statistics -o /dev/null ${translation}
                 VERBATIM
             )
     endforeach()
